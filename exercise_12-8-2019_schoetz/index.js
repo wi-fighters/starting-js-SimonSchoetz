f486dc9e476be1cdba1c3b8b0cbed9c1903c94b1
Simon <simonsch.tz@gmail.com>
2019-08-12 16:10:15 +0200

    Added more structure

--- exercise_12-8-2019_schoetz/index.js
+++ exercise_12-8-2019_schoetz/index.js
@@ -1,15 +1,22 @@
 //Java exercise from 12th Aug 2019
 //Author: Simon Schötz
-/*let firstName = "Simon";
-let lastName = "Schötz";
-let city = "Berlin";
-let age = 29;
-let genre = "Dubstep";*/
 
-//console.log(firstName, lastName ,city, age, genre);
-//console.log(firstName, lastName+" is a "+age+" years old "+genre+" producer from "+city);
-let length = 16.78; // Number
-let lastName = "Johnson";  //String
-let car = ""; //Stringn
-let isOpen = true; //
-typeof(isOpen);
+//Step 1
+/*let firstName = "Simon";
+let lastName = "Schötz";*/
+
+//Step 2
+/*let city = "Berlin";
+let age = 29;
+let genre = "Dubstep";
+console.log(firstName, lastName ,city, age, genre);*/
+
+//Step 3
+/*console.log(firstName, lastName+" is a "+age+" years old "+genre+" producer from "+city);*/
+
+//Step 4
+/*let length = 16.78; // Number
+let lastName = "Johnson";  // String
+let car = ""; // String
+let isOpen = true; // Boolean*/
+
