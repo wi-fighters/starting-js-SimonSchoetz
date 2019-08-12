d0d42a070e17d78c6e4ba4e2ee34770ac59828a7
Simon <simonsch.tz@gmail.com>
2019-08-12 16:06:38 +0200

    Executed Step4

--- exercise_12-8-2019_schoetz/index.js
+++ exercise_12-8-2019_schoetz/index.js
@@ -1,10 +1,15 @@
 //Java exercise from 12th Aug 2019
 //Author: Simon Schötz
-let firstName = "Simon";
+/*let firstName = "Simon";
 let lastName = "Schötz";
 let city = "Berlin";
 let age = 29;
-let genre = "Dubstep";
+let genre = "Dubstep";*/
 
 //console.log(firstName, lastName ,city, age, genre);
-console.log(firstName, lastName+" is a "+age+" years old "+genre+" producer from "+city);
+//console.log(firstName, lastName+" is a "+age+" years old "+genre+" producer from "+city);
+let length = 16.78; // Number
+let lastName = "Johnson";  //String
+let car = ""; //Stringn
+let isOpen = true; //
+typeof(isOpen);
