a76858e82ecd2f1a2b7debc9053d7b5749a137be
Simon <simonsch.tz@gmail.com>
2019-08-13 13:59:58 +0200

    Executed Pt1

--- exercise_13-8-2019_schoetz/index.js
+++ exercise_13-8-2019_schoetz/index.js
@@ -1,3 +1,8 @@
 //Exercise: Mathmatical exercises with JavaScript
 //Author: Simon Schötz
 
+//1 Percent
+
+let everything = 30;
+let justAPart = 17;
+console.log("result: " + justAPart/ (everything/100) + "%");
