68bf7444bbf8f51017d5e3f655b42a1f96fb346c
Simon <simonsch.tz@gmail.com>
2019-08-13 14:14:11 +0200

    Executed Pt2

--- exercise_13-8-2019_schoetz/index.js
+++ exercise_13-8-2019_schoetz/index.js
@@ -2,7 +2,13 @@
 //Author: Simon Schötz
 
 //1 Percent
-
+/*
 let everything = 30;
 let justAPart = 17;
 console.log("result: " + justAPart/ (everything/100) + "%");
+*/
+
+//Working with Averages
+
+console.log( (hours+8+6+5+9+8+2+1+8.5+7)/9 );
+//--> Average hours  6.055555
