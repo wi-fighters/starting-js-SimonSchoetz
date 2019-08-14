c732233d966d08690eaaa9b96f05c1779fb493a4
Simon <simonsch.tz@gmail.com>
2019-08-14 10:05:50 +0200

    Exectued Pt2

--- exercise_14-08-2019-schoetz/index.js
+++ exercise_14-08-2019-schoetz/index.js
@@ -2,8 +2,15 @@
 //Author: Simon Schötz
 
 // canDrive
-
-const canDrive = 18
-const driverAge = 17
-const ageCheck = (driverAge>=canDrive) ? "You're good, move on!" : "Gtfo the street you crazy kid!"
+/*
+const canDrive = 18;
+const driverAge = 17;
+const ageCheck = (driverAge>=canDrive) ? "You're good, move on!" : "Gtfo the street you crazy kid!";
 console.log(ageCheck)
+*/
+
+// Slow Down
+const maxSpeed = 130;
+const driverSpeed = 160;
+const speedCheck = (maxSpeed >= driverSpeed) ? "Below 130km/h! My man!" : "Slow down you crazy son of a bitch, it's a max of 130km/h here!";
+console.log(speedCheck)
