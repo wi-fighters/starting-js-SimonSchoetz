1accae6227ddca7f5fb49361829565adb7b3f922
Simon <simonsch.tz@gmail.com>
2019-08-14 10:09:07 +0200

    Exectued Pt3

--- exercise_14-08-2019-schoetz/index.js
+++ exercise_14-08-2019-schoetz/index.js
@@ -1,7 +1,7 @@
 //Exercise: Ternary Worsksheet
 //Author: Simon Schötz
 
-// canDrive
+// 1 canDrive
 /*
 const canDrive = 18;
 const driverAge = 17;
@@ -9,8 +9,16 @@ const ageCheck = (driverAge>=canDrive) ? "You're good, move on!" : "Gtfo the str
 console.log(ageCheck)
 */
 
-// Slow Down
+// 2 Slow Down
+/*
 const maxSpeed = 130;
 const driverSpeed = 160;
 const speedCheck = (maxSpeed >= driverSpeed) ? "Below 130km/h! My man!" : "Slow down you crazy son of a bitch, it's a max of 130km/h here!";
 console.log(speedCheck)
+*/
+
+// 3 How old are you?
+const age = 23;
+const minAge = 21;
+const personAge = (age >= minAge) ? "What's your poison?" : "Here you go, enjoy your milk.";
+console.log(personAge)
