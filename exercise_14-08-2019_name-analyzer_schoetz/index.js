ee40ee9c22ed8558d789708fd0e9bc41bd8d7002
Simon <simonsch.tz@gmail.com>
2019-08-15 09:25:01 +0200

    With Bonus Pt1

--- exercise_14-08-2019_name-analyzer_schoetz/index.js
+++ exercise_14-08-2019_name-analyzer_schoetz/index.js
@@ -1,8 +1,17 @@
 // Name Analyzer Exercise done allone :(
 //Author: Simon Schötz
-
+/*
 const name = "Winston";
 console.log(`Hello ${name}.
 Your name is ${name.length} letters long.
 The first letter of your name is ${name[0]}, the last is ${name[name.length -1]}.
 If I wanted to be funny, I would call you ${name.substring(0, 4)}chmoo. `);
+*/
+
+const name = "Winston Churchill";
+const nameWOSpace = name.replace(/\s/g, "");
+console.log(`Hello ${name}.
+Your name is ${nameWOSpace.length} letters long.
+The first letter of your name is ${name[0]}, the last is ${name[name.length -1]}.
+If I wanted to be funny, I would call you ${name.substring(0, 4)}chmoo. `);
+
