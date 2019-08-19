459ce3fc636907f85f7f0f2e82a0bff12bb42883
Simon <simonsch.tz@gmail.com>
2019-08-19 15:49:42 +0200

    Exectued Pt3

--- exercise_19-08-2019_schoetz-math-objects/index.js
+++ exercise_19-08-2019_schoetz-math-objects/index.js
@@ -1,7 +1,7 @@
 // Some more math objects
 // Author Simon Schötz
 
-//1 Lowest Number
+// 1 Lowest Number
 /*
 const numOne = 5;
 const numTwo = 32397;
@@ -9,9 +9,23 @@ const numThree = 39;
 console.log(Math.min(numOne, numTwo, numThree,));
 */
 
-//2 Lowest Number
-
+// 2 Lowest Number
+/*
 const numOne = 5;
 const numTwo = 32397;
 const numThree = 39;
 console.log(Math.max(numOne, numTwo, numThree,));
+*/
+
+// 3 Rounding
+
+console.log(Math.ceil (321.32321));
+console.log(Math.ceil (326.76));
+console.log(Math.ceil (76788.7));
+console.log(Math.ceil (-9.78));
+console.log(Math.ceil (43.342));
+console.log(Math.floor (3321.32321));
+console.log(Math.floor (326.76));
+console.log(Math.floor (76788.7));
+console.log(Math.floor (-9.78));
+console.log(Math.floor (28.329));
