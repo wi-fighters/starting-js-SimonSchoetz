f529c3cf17aebb8bde1a92c2789434c0e7184f8c
Simon <simonsch.tz@gmail.com>
2019-08-19 15:44:37 +0200

    Exectued Pt2

--- exercise_19-08-2019_schoetz-math-objects/index.js
+++ exercise_19-08-2019_schoetz-math-objects/index.js
@@ -1,9 +1,17 @@
 // Some more math objects
 // Author Simon Schötz
 
-//Lowest Number
+//1 Lowest Number
+/*
 const numOne = 5;
 const numTwo = 32397;
 const numThree = 39;
 console.log(Math.min(numOne, numTwo, numThree,));
+*/
 
+//2 Lowest Number
+
+const numOne = 5;
+const numTwo = 32397;
+const numThree = 39;
+console.log(Math.max(numOne, numTwo, numThree,));
