9be2cd8286986201469badcdf543c598863982af
Simon <simonsch.tz@gmail.com>
2019-08-19 15:26:58 +0200

    first commit new exercise

---
+++ exercise_19-08-2019_schoetz-math-objects/index.js
@@ -0,0 +1,9 @@
+// Some more math objects
+// Author Simon Schötz
+
+//Lowest Number
+const numOne = 5;
+const numTwo = 32397;
+const numThree = 39;
+console.log(Math.min(numOne, numTwo, numThree,));
+
