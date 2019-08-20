51498bef763b93886d428b49efefa37fa39129c0
Simon <simonsch.tz@gmail.com>
2019-08-20 16:08:26 +0200

    fixed Name Analyzer exercise

--- exercise_14-08-2019_name-analyzer_schoetz/index.js
+++ exercise_14-08-2019_name-analyzer_schoetz/index.js
@@ -1,24 +1,31 @@
 // Name Analyzer Exercise done allone :(
 //Author: Simon Schötz
-/*
+
 const name = "Winston";
 console.log(`Hello ${name}.
 Your name is ${name.length} letters long.
-The first letter of your name is ${name[0]}, the last is ${name[name.length -1]}.
+The first letter of your name is ${name[0]}, the last is ${
+  name[name.length - 1]
+}.
 If I wanted to be funny, I would call you ${name.substring(0, 4)}chmoo. `);
-*/
+
 /*
 const name = "Winston Churchill";
 const nameWOSpace = name.replace(/\s/g, "");
 console.log(`Hello ${name}.
 Your name is ${nameWOSpace.length} letters long.
-The first letter of your name is ${name[0]}, the last is ${name[name.length -1]}.
+The first letter of your name is ${name[0]}, the last is ${
+  name[name.length - 1]
+}.
 If I wanted to be funny, I would call you ${name.substring(0, 4)}chmoo. `);
-*/
+
 const name = "    Winston Churchill    ";
 const nameWOSpace = name.replace(/\s/g, "");
 const nameTrim = name.trim();
 console.log(`Hello ${nameTrim}.
 Your name is ${nameWOSpace.length} letters long.
-The first letter of your name is ${name[0]}, the last is ${name[name.length -1]}.
-If I wanted to be funny, I would call you ${name.substring(0, 4)}chmoo. `);
+The first letter of your name is ${nameTrim[0]}, the last is ${
+  nameTrim[nameTrim.length - 1]
+}.
+If I wanted to be funny, I would call you ${nameTrim.substring(0, 4)}chmoo. `);
+*/
