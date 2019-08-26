197c786399e83a30a1659b8f795abeaf42d1d121
Simon <simonsch.tz@gmail.com>
2019-08-26 14:00:04 +0200

    Executed Pt7&8

--- exercise_26-08-2019_schoetz_Changing-variables/index.js
+++ exercise_26-08-2019_schoetz_Changing-variables/index.js
@@ -1,23 +1,29 @@
 //Changing variables exercise
 //Author: Simon Schötz
 
-//1
+// 1
 
 //let name = "Simon Schötz";
 //name = "You wot m8!";
 //console.log(name);
 
-//2
+// 2
 //console.log("Schm" + name);
 
-//3 It still will be a string but adding "Schm" doesnt change the value of name
+// 3 It still will be a string but adding "Schm" doesnt change the value of name
 //console.log(typeof "Schm" + name);
 
-//4
+// 4
 //console.log(name); //Well, the first value is reassigned by the reassign thingy so actually, there are no 2 different values at the end..... . . . .. .
 
-//5 & 6
+// 5 & 6
 let coffee = 290;
 let x = "Eva";
 let y = 34;
-console.log(`${x} drinks ${y} cups of coffee per day.`);
+let daysBeforeExam = 13;
+//console.log(`${x} drinks ${y} cups of coffee per day.`);
+
+// 7 & 8
+console.log(
+  `${x} drinks ${y} cups of coffee per day with in ${daysBeforeExam} days.`
+);
